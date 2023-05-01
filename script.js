2e376fb4876e0c8bcca62d654dd9b3714e6e41ce
Ashfaque Hossain Abir <108189537+Abirett3@users.noreply.github.com>
2023-05-01 12:20:39 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+        if(input.value.length === false) {
+            ul.style.display = "none"
+        } else {
+            ul.style.display = "block"
+        }
+    }
+    if(input.value == '') {
+        ul.style.display = "none"
+    }
+}
